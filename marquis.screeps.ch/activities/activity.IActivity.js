56176fc1f53ca502a9c3062896529b332b4b655d
LukasWillin <lukas.willin@students.fhnw.ch>
2019-01-09 22:04:26 +0100

    <1> Improve interface IInstruction <2> Introduce initCache interface function definitions for IInstruction and IActivity <TODO> 1: Add activity.IActivityEntity, 2: Clean up kernel.IActivityEntity and IActivityExecution state to achieve more defined entity classes (Seperation of concerns)

--- marquis.screeps.ch/activities/activity.IActivity.js
+++ marquis.screeps.ch/activities/activity.IActivity.js
@@ -1,35 +1,50 @@
 
 
 const NotImplementedError = require('./error/NotImplementedError');
-const Instruction = require('./activity.instruction.Instruction');
-const InstructionFunction = require('./activity.instruction.InstructionFunction');
+const IInstruction = require('./activity.instruction.IInstruction');
+const IInstructionFunction = require('./activity.instruction.IInstructionFunction');
 const IInstructionSet = require('./activity.instruction.IInstructionSet');
 
 /**
  * An activity implements the logic to execute specific task(s).
- * An activity must at least implement one instruction to be run by the scheduler
- * with the name 'run'.
+ * 
  * The activity class never creates instances but implements the static instructions
- * to execute upon an activity state.
+ * to execute upon an activity entity.
+ * 
+ * @public @class
  * @implements {IInstructionSet}
+ * @property {IInstruction} run - An activity must at least implement one instruction to be run by the scheduler
+ *      with the property name 'run'.
  */
 class IActivity {
     
 }
 
 Object.defineProperties(IActivity, {
-    /**
-     * Root function at which the execution starts or resumes from.
+    
+    /** Root instruction at which the execution starts or resumes from.
+     * @static @public 
+     * @type {IInstruction}
      */
-    run: { 
-        value: new Instruction(
-            function instr0(ths, scope, cache, call, err, ...args) { 
-                throw new NotImplementedError("Interface implementation of IActivity#run");
-            }
+    static run: { 
+        value: new IInstruction(
+            new IInstructionFunction((ths, scope, cache, call, err, ...args) => { 
+                throw new NotImplementedError("Interface IActivity#run must be overridden.");
+            })
         )
     },
-    initCache(cache) {
 
+    /** Initializes given cache object with required data to run a certain instruction.
+     * The init cache is ran once before the first call to any instruction.
+     * @static @public
+     * @function
+     * @param {IActivityEntity} activityEntity - The persisted activity entity from memory.
+     * @param {IInstruction} nextInstruction - The instruction which will be called next. 
+     * @param {Object} cache - An empty object to initialize.
+     *      This object is a shortcut to 'activityInstance.cache'.
+     */
+    static initCache(activityEntity, nextInstruction, cache) {
+        throw new NotImplementedError("Interface IActivity#initCache must be overridden.");
     }
 });
 
--- marquis.screeps.ch/activities/activity.instruction.IInstruction.js
+++ marquis.screeps.ch/activities/activity.instruction.IInstruction.js
@@ -2,31 +2,48 @@
 
 /**
  * An instruction is an ordered list of instruction functions.
- * @type {Array<InstructionFunction>}
+ * 
+ *
+ * @public @interface
+ * @type {Array<IInstructionFunction>}
+ * @example
+ *  const instruction = new IInstruction(
+ *      // First instruction function (index:=0) receives the arguments of the caller.
+ *      new IInstructionFunction((ths, scope, cache, call, err, ...args) => {
+ *          scope.arg0 = args[0];
+ *          scope.arg1 = args[1];
+ *
+ *          call(SomeActivity.anotherInstruction, "any", "arg", {}, 8);
+ *      }),
+ *      // Later instruction function may receive return results from a previously called instruction.
+ *      // Note that invocations of other instructions through 'call' will always require a split of an instruction
+ *      // function into two.
+ *      new IInstructionFunction((ths, scope, cache, call, err, ...args) => {
+ *          if (err) return err;
+ *           
+ *          scope.returnValue = args[0];
+ *
+ *          scope.out = 1234;
+ *          return true;
+ *      })
+ *  );
  */
 class IInstruction extends Array {
-    constructor(...instructionFunctions) { }   
+    
+    constructor(...instructionFunctions) { }
+    
+    /**
+     * @param {IActivityEntity} ths - The activity entity.
+     * @param {Object} cache - An empty object to use as instruction function cache.
+     *      This cache should be initialized. Returned values are ignored.
+     * @param {Object} scope - Persisted Instruction scope
+     * @param {int} nextFnIndex - The instruction function which will be executed.
+     *      Can be used to prevent unnecessary initialization when instruction is resumed and some
+     *      Data isnt be needed.
+     */
+    initCache(ths, cache, scope, nextFnIndex) {
+
+    }
 }
 
 module.exports = IInstruction;
-
-/*
-    @example
-
-    const Instruction = new Instruction(
-        new IInstructionFunction((ths, scope, cache, call, err, ...args) => {
-            scope.arg0 = args[0];
-            scope.arg1 = args[1];
-
-            call(SomeActivity.anotherInstruction, "any", "arg", {}, 8);
-        }),
-        new IInstructionFunction((ths, scope, cache, call, err, ...args) => {
-            if (err) return err;
-            
-            scope.returnValue = args[0];
-
-            scope.out = 1234;
-            return true;
-        })
-    );
-*/
